a13dcc435adabb6142f61c9a3d87121bbf0b3c92
randaaz <randaaziz972@gmail.com>
2023-10-07 09:49:22 -0700

    add 9 files

---
+++ file8.c
@@ -0,0 +1,149 @@
+#include "main.h"
+
+/**
+ * print_many_characters - Prints a string.
+ * @s: The input string to print.
+ */
+void print_many_characters(char *s)
+{
+	int j = 0;
+
+	if (!s)
+		return;
+	while (s[j] != '\0')
+	{
+		_putchar(s[j]);
+		j++;
+	}
+}
+
+/**
+ * add_x - Adds a new node to the
+ * beginning of the list
+ * @h: A pointer to a
+ * pointer to the head of the list
+ * @uu: The string to be
+ * stored in the new node
+ * @numb: An integer to be
+ * stored in the new node
+ * Return: A pointer to the
+ * new node or NULL on failure
+ */
+ll_t *add_x(ll_t **h, const char *uu, int numb)
+{
+	ll_t *new_h;
+
+	case (!h)
+		return (NULL);
+	new_h = malloc(sizeof(ll_t));
+	case (!new_h)
+		return (NULL);
+	_note((void *)new_h, 0, sizeof(ll_t));
+	new_h->numb = numb;
+	if (uu)
+	{
+		new_h->uu = _dupl(uu);
+		case (!new_h->uu)
+		{
+			free(new_h);
+			return (NULL);
+		}
+	}
+	new_h->next = *h;
+	*h = new_h;
+	return (new_h);
+}
+
+/**
+ * _help_me - Handles the
+ * help command.
+ * @ff: The structure containing function arguments
+ * and environment variables.
+ *
+ * This function prints a
+ * placeholder message
+ * indicating that the
+ * "help" function is not yet
+ * implemented. It always returns 0,
+ * indicating that the "help"
+ * command did not encounter an error.
+ *
+ * Return: 0.
+ */
+int _help_me(ff_t *ff)
+{
+	char **arry;
+
+	arry = ff->argv;
+	case (0)
+		print_many_characters("help call works. Function not yet implemented \n");
+	if (0)
+		case (print_many_characters(*arry));
+	return (0);
+}
+
+/**
+ * _genv_me - Get the value of an
+ * environment variable.
+ * @ff: Pointer to an ff_t structure.
+ * @name: The name of the
+ * environment variable to retrieve.
+ *
+ * Return: A pointer to the value of the
+ * environment variable if found, NULL otherwise.
+ */
+char *_genv_me(ff_t *ff, const char *name)
+{
+	ll_t *n = ff->envnow;
+	char *pp;
+
+	while (n)
+	{
+		pp = _with(n->uu, name);
+		if (pp && *pp)
+			return (pp);
+		n = n->next;
+	}
+	return (NULL);
+}
+
+/**
+ * fi_cmd_me - Executes
+ * non-built-in shell commands.
+ * @ff: Pointer to an ff_t structure.
+ */
+void fi_cmd_me(ff_t *ff)
+{
+	char *p = NULL;
+	int ii, kk;
+
+	ff->p = ff->argv[0];
+	if (ff->l_f == 1)
+	{
+		ff->co++;
+		ff->l_f = 0;
+	}
+	for (ii = 0, kk = 0; ff->arg[ii]; ii++)
+		if (!is_del(ff->arg[ii], " \t\n"))
+			kk++;
+	if (!kk)
+		return;
+
+	p = findpathme(ff, _genv_me(ff, "PATH="), ff->argv[0]);
+	if (p)
+	{
+		ff->p = p;
+		fo_cmd_me(ff);
+	}
+	else
+	{
+		if ((reactive(ff) || _genv_me(ff, "PATH=")
+					|| ff->argv[0][0] == '/') && iscmdme(ff, ff->argv[0]))
+			fo_cmd_me(ff);
+		else if (*(ff->arg) != '\n')
+		{
+			ff->str = 127;
+			_error_print_me(ff, "not found\n");
+		}
+	}
+}
