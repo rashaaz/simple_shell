f899c6440f970657a8b08cd9e3bdc8a550958afc
rashaaz <rashaabdelaziz111@gmail.com>
2023-10-07 22:07:35 -0700

    modfy

--- file1.c
+++ file1.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _len - Computes the length of a string
  * @c: The input string to measure
@@ -16,12 +15,14 @@ int _len(char *c)
 		return (0);
 	}
 
-	while (*c++)
+	for (; *c;)
 	{
 		j++;
+		c++;
 	}
 	return (j);
 }
+
 /**
  * split1 - Splits a string into
  * words based on
@@ -77,7 +78,6 @@ char **split1(char *si, char del)
  * @s: The string representing the alias
  * Return: 1 if an error occurred, 0 otherwise
  */
-
 int u_alias_me(ff_t *ff, char *s)
 {
 	char *po, x;
@@ -126,6 +126,7 @@ int _alias_me(ff_t *ff)
 
 	return (0);
 }
+
 /**
  * a_n_end - Adds a new node
  * to the end of the list
@@ -138,7 +139,6 @@ int _alias_me(ff_t *ff)
  * Return: A pointer to the new
  * node or NULL on failure
  */
-
 ll_t *a_n_end(ll_t **h, const char *uu, int numb)
 {
 	ll_t *new_n, *n;
